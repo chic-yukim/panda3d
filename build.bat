497e07f5dd3c2a9318b3c5fdb9a471e72eeb8f69
Younguk Kim <bluekyu.dev@gmail.com>
2017-07-04 09:39:20 +0900

    Update build batch file

--- build.bat
+++ build.bat
@@ -1,1 +1,11 @@
-thirdparty\win-python3.5-x64\python.exe makepanda\makepanda.py --threads 8 --optimize 4 --msvc-version=14 --windows-sdk=10.0.14393.0 --nothing --use-direct --use-gl --use-eigen --use-openal --use-vorbis --use-zlib --use-png --use-jpeg --use-squish --use-freetype --use-assimp --use-egg --use-pandatool --use-sse2
+@echo off
+
+SET PANDA_PYTHON_PATH=thirdparty\win-python3.5-x64\python.exe 
+SET THREADS=8
+SET OPTIMIZE=4
+SET MSVC_VERSION=14.1
+SET WINDOWS_SDK=10.0.15063.0
+
+SET EXTRA_INPUT=--nothing --use-direct --use-gl --use-eigen --use-openal --use-vorbis --use-zlib --use-png --use-jpeg --use-squish --use-freetype --use-assimp --use-egg --use-pandatool --use-sse2
+
+%PANDA_PYTHON_PATH% makepanda\makepanda.py --threads %THREADS% --optimize %OPTIMIZE% --msvc-version=%MSVC_VERSION% --windows-sdk=%WINDOWS_SDK% %EXTRA_INPUT%
